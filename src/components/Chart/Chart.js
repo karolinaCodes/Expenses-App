472faf6afe4317e2184f1c3263eade08c8245463
karolinaCodes <karolinaswislocki@hotmail.com>
2021-08-20 13:35:25 -0400

    First commit

---
+++ src/components/Chart/Chart.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+import ChartBar from "./ChartBar";
+import "./Chart.css";
+
+const Chart = (props) => {
+  const dataPointValues = props.dataPoints.map((dataPoint) => dataPoint.value);
+  //^creates an array of all the dataPoint values
+  const totalMaximum = Math.max(...dataPointValues);
+  //^.max only accepts a list of numbers so add the array of values and spreads them as single arguments
+
+  return (
+    <div className="chart">
+      {props.dataPoints.map((dataPoint) => (
+        <ChartBar
+          key={dataPoint.label}
+          value={dataPoint.value}
+          maxValue={totalMaximum}
+          label={dataPoint.label}
+        />
+      ))}
+    </div>
+  );
+};
+
+export default Chart;
